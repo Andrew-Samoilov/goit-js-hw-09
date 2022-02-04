2386b6fc7aff310219ed5ab3e4420d7f42539cfe
Andrew Samoilov <veprsag@gmail.com>
2022-02-04 18:25:17 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,4 @@
-console.log(`7777777`);
+console.log(`1111`);
 import Notiflix from 'notiflix';
 const refs = {
   formY: document.querySelector('.form'),
@@ -6,8 +6,6 @@ const refs = {
   delay: document.querySelector('input[name="delay"]'),
   steps: document.querySelector('input[name="step"]'),
 };
-
-// console.log(` delay `, refs.delay);
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
@@ -17,7 +15,7 @@ function createPromise(position, delay) {
         resolve({ position, delay });
       }
       reject({ position, delay });
-    }, delay);yy
+    }, delay);
   });
 }
 
@@ -27,32 +25,35 @@ function onFormSubmit(evt) {
   evt.preventDefault();
   // console.log(`inside form submit`);
   let amount = refs.amount.value;
-  let delay = refs.delay.value;
+  let delay = Number(refs.delay.value);
   let steps = refs.steps.value;
+  console.log(` Delay `, delay);
 
   createPromise(1, delay)
     .then(({ position, delay }) => {
-      // console.log(`✅ Fulfilled promise 1 in ${delay}ms`);
-      Notiflix.Notify.success(`✅ Fulfilled promise 1 in ${delay}ms`);
+      console.log(` delay `, delay);
+      console.log(`✅ Fulfilled promise 1 in ${delay} ms`);
+      Notiflix.Notify.success(`✅ Fulfilled promise 1 in ${delay} ms`);
     })
     .catch(({ position, delay }) => {
-      // console.log(`❌ Rejected promise 1 in ${delay}ms`);
+      console.log(` delay `, delay);
+      console.log(`❌ Rejected promise 1 in ${delay}ms`);
       Notiflix.Notify.failure(`❌ Rejected promise 1 in ${delay}ms`);
     });
 
-  // console.log(` delay `, delay);
+  console.log(` delay `, delay);
 
   for (let i = 2; i <= amount; i++) {
-    delay = Number(steps * i);
-    // console.log(`i ${i} delay `, delay);
+    let delay = Number(steps * i);
+    console.log(`i ${i} delay `, delay);
 
     createPromise(i, delay)
       .then(({ position, delay }) => {
-        // console.log(`✅ Fulfilled promise ${i} in ${delay}ms`);
+        console.log(`✅ Fulfilled promise ${i} in ${delay}ms`);
         Notiflix.Notify.success(`✅ Fulfilled promise ${i} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
-        // console.log(`❌ Rejected promise ${i} in ${delay}ms`);
+        console.log(`❌ Rejected promise ${i} in ${delay}ms`);
         Notiflix.Notify.failure(`❌ Rejected promise ${i} in ${delay}ms`);
       });
   }
